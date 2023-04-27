c080922900a4ef5eee344443f197a3e75f274155
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-04-27 18:52:06 +0300

    Updated prototype file

--- shell.h
+++ shell.h
@@ -21,14 +21,10 @@
 #define DONT_CREATE_NEW_CHILD_PROCESS 6
 #define LAUNCH_RUNTIME_PROCESS 7
 
-
-extern char **environ;
-
-extern int status;
-
-extern int lineNumber;
-
 extern char *nameOfShell;
+extern int status;
+extern int lineNumber;
+extern char **environ;
 
 /**
  * struct Alias - aliases linked list
@@ -45,64 +41,34 @@ typedef struct Alias
 } alias;
 
 void displayErrorMessage(char *arg0, char *arg1);
-
 char *checkIfNotBuiltIn(char **arguements);
-
 char *inputCleaner(char *previousBuffer, size_t *previousSize);
-
 char *checkIfArgumentIsVariable(char *arguement);
-
 char *_itoa(int integer);
-
 char *copyString(char *source);
-
 char *concatenateString(char *firstString, char *secondString);
-
 char *findArrayElement(char **array, char *itemName);
-
 char **createArray(char *string, char endOfListEntryChar, char **ifSemiColonPointer);
-
 char **duplicateArray(char **previousArray, int createdArraySize);
-
 int releaseArray(char **arguements);
-
 int configureEnvironmentVariable(const char *name, const char *value);
-
 int runCommand(char **arguements);
-
 int stringToInteger(char *string);
-
 int errorCheckerForInput(char *pointer);
-
 int getStringLength(char *string);
-
 int lengthOfList(char **testList, char *indexedEntry);
-
 int compareString(char *firstString, char *secondString, int pref_or_match);
-
 int deleteEnvironmentVariable(const char *name);
-
 int commandRuntimeHandler(char **arguements);
-
 int checkIfCommandInbuilt(char **arguements);
-
 int logicalOperatorHandler(char **arguements, char operator, int last_compare);
-
 int changeDirectory(char *name);
-
 int commandAliasProcessing(char **arguements, int free);
-
 int releaseAlias(alias *aliasPointer);
-
 int replaceIfAlias(char **arguements, alias *aliasPointer);
-
 int displayAliases(alias *aliasPointer);
-
 int displayAliasValue(char *arguement, alias *aliasPointer);
-
 int configureAlias(char *arguement, alias *aliasPointer, char *setValue);
-
 int displayEnvironMentVariable(void);
-
 
 #endif
